25584a8648e52da0aaf423783504009024a4eba6
Benjamin Höglinger <nefarius@dhmx.at>
2017-09-15 22:37:24 +0200

    Added PSM definitions

--- AirBender/L2CAP.h
+++ AirBender/L2CAP.h
@@ -1,6 +1,6 @@
 #pragma once
 
-typedef enum _L2CAP_CODE
+typedef enum _L2CAP_SIGNALLING_COMMAND_CODE
 {
     L2CAP_Reserved = 0x00,
     L2CAP_Command_Reject = 0x01,
@@ -45,10 +45,19 @@ typedef enum _L2CAP_CODE
     L2CAP_Echo_Response = 0x09,
     L2CAP_Information_Request = 0x0A,
     L2CAP_Information_Response = 0x0B
-} L2CAP_CODE;
+} L2CAP_SIGNALLING_COMMAND_CODE;
 
-#define L2CAP_IS_CONTROL_CHANNEL(_buf_)     ((BOOLEAN)_buf_[6] == 0x01 && _buf_[7] == 0x00)
-#define L2CAP_IS_HID_INPUT_REPORT(_buf_)    ((BOOLEAN)_buf_[8] == 0xA1 && _buf_[9] == 0x01)
+typedef enum _L2CAP_PSM
+{
+    L2CAP_PSM_HID_Service = 0x01,
+    L2CAP_PSM_HID_Command = 0x11,
+    L2CAP_PSM_HID_Interrupt = 0x13
+} L2CAP_PSM;
+
+#define L2CAP_IS_CONTROL_CHANNEL(_buf_)                     ((BOOLEAN)_buf_[6] == 0x01 && _buf_[7] == 0x00)
+#define L2CAP_IS_HID_INPUT_REPORT(_buf_)                    ((BOOLEAN)_buf_[8] == 0xA1 && _buf_[9] == 0x01)
+#define L2CAP_GET_SIGNALLING_COMMAND_CODE(_buf_)            ((L2CAP_SIGNALLING_COMMAND_CODE)_buf_[8])
+#define L2CAP_GET_PROTOCOL_SERVICE_MULTIPLEXER(_buf_)       ((L2CAP_PSM)_buf_[12])
 
 BOOLEAN FORCEINLINE L2CAP_IS_SIGNALLING_COMMAND_CODE(
     PUCHAR Buffer
