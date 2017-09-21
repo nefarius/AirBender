a8887a1fe88c9b48917462cd70a2bbe5b73f3db1
Benjamin Höglinger <nefarius@dhmx.at>
2017-09-21 21:52:46 +0200

    Addiing IOCTL codes

--- Include/AirBender.h
+++ Include/AirBender.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#define IOCTL_INDEX             0x800
+#define FILE_DEVICE_AIRBENDER   65500U
+
+#define IOCTL_AIRBENDER_CHILDREN_GET_COUNT      CTL_CODE(FILE_DEVICE_AIRBENDER  \
+                                                            IOCTL_INDEX,        \
+                                                            METHOD_BUFFERED,    \
+                                                            FILE_READ_ACCESS)
+
+#define IOCTL_AIRBENDER_GET_HID_REPORT          CTL_CODE(FILE_DEVICE_AIRBENDER  \
+                                                            IOCTL_INDEX,        \
+                                                            METHOD_BUFFERED,    \
+                                                            FILE_READ_ACCESS | FILE_WRITE_ACCESS)
