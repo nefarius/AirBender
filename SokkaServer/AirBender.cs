9617ba034166ee6eacca0daa9554c2e0742984b9
Benjamin Höglinger <nefarius@dhmx.at>
2017-09-21 21:37:24 +0200

    More service stuff

--- SokkaServer/AirBender.cs
+++ SokkaServer/AirBender.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PInvoke;
 
 namespace SokkaServer
 {
     class AirBender
     {
         public static Guid ClassGuid => Guid.Parse("a775e97e-a41b-4bfc-868e-25be84643b62");
+
+        public AirBender(string devicePath)
+        {
+            DevicePath = devicePath;
+
+            DeviceHandle = Kernel32.CreateFile(DevicePath,
+                Kernel32.ACCESS_MASK.GenericRight.GENERIC_READ | Kernel32.ACCESS_MASK.GenericRight.GENERIC_WRITE,
+                Kernel32.FileShare.FILE_SHARE_READ | Kernel32.FileShare.FILE_SHARE_WRITE,
+                IntPtr.Zero, Kernel32.CreationDisposition.OPEN_EXISTING,
+                Kernel32.CreateFileFlags.FILE_ATTRIBUTE_NORMAL | Kernel32.CreateFileFlags.FILE_FLAG_OVERLAPPED,
+                Kernel32.SafeObjectHandle.Null
+            );
+
+            if (DeviceHandle.IsInvalid)
+                throw new ArgumentException($"Couldn't open device {DevicePath}");
+        }
+
+        ~AirBender()
+        {
+            DeviceHandle.Close();
+        }
+
+        public string DevicePath { get; }
+
+        private Kernel32.SafeObjectHandle DeviceHandle { get; }
     }
 }
--- SokkaServer/SokkaService.cs
+++ SokkaServer/SokkaService.cs
@@ -1,21 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Nefarius.Devcon;
+using Serilog;
 
 namespace SokkaServer
 {
     class SokkaService
     {
+        private AirBender BthHost { get; set; }
+
         public void Start()
         {
-            
+            Log.Information("SokkaService started");
+
+            string path = string.Empty, instance = string.Empty;
+
+            if (Devcon.Find(AirBender.ClassGuid, ref path, ref instance))
+            {
+                Log.Information($"Found AirBender device {path} ({instance})");
+
+                BthHost = new AirBender(path);
+            }
         }
 
         public void Stop()
         {
-            
+
         }
     }
 }
