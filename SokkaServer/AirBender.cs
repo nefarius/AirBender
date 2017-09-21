c52b24e94d5269dcff43a009f971b2bfd93a7e2d
Benjamin Höglinger <nefarius@dhmx.at>
2017-09-21 20:58:03 +0200

    Added icon Added Airbender GUID

---
+++ SokkaServer/AirBender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokkaServer
+{
+    class AirBender
+    {
+        public static Guid ClassGuid => Guid.Parse("a775e97e-a41b-4bfc-868e-25be84643b62");
+    }
+}
