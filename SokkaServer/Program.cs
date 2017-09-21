b45c1b92aaa6964846ae99ede0c99eb652550aff
Benjamin Höglinger <nefarius@dhmx.at>
2017-09-21 20:47:40 +0200

    Added 3rd party stuff

--- SokkaServer/Program.cs
+++ SokkaServer/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Topshelf;
 
 namespace SokkaServer
 {
@@ -10,6 +6,20 @@ namespace SokkaServer
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(x =>                                 
+            {
+                x.Service<SokkaService>(s =>                     
+                {
+                    s.ConstructUsing(name => new SokkaService());
+                    s.WhenStarted(tc => tc.Start());             
+                    s.WhenStopped(tc => tc.Stop());              
+                });
+                x.RunAsLocalSystem();                            
+
+                x.SetDescription("Enables communication with AirBender Bluetooth Host Devices.");        
+                x.SetDisplayName("SokkaServer");                       
+                x.SetServiceName("SokkaServer");                                
+            });
         }
     }
 }
---
+++ SokkaServer/SokkaService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokkaServer
+{
+    class SokkaService
+    {
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
